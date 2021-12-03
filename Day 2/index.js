3f4dd44aa1f27df46f850ea7c24ae0a7e5f5258b
Connor Roberts <57920178+robertscl@users.noreply.github.com>
2021-12-02 20:59:24 -0500

    Update index.js

--- Day 2/index.js
+++ Day 2/index.js
@@ -2,11 +2,8 @@ const fs = require("fs");
 
 const text = fs.readFileSync("./input.txt", "utf-8");
 
-const data = text.split("\n");
-
-const dataDict = data.map((item) => {
-  const [dir, val] = item.split(" ");
-  return [dir, val];
+const data = text.split("\n").map((item) => {
+  return item.split(" ");
 });
 
 // Part One:
@@ -15,20 +12,21 @@ const dataDict = data.map((item) => {
 let horizontal = 0;
 let depth = 0;
 
-dataDict.forEach((direction) => {
+data.forEach((direction) => {
   switch (direction[0]) {
     case "up":
-      depth = depth - Number(direction[1]);
+      depth -= Number(direction[1]);
       break;
     case "down":
-      depth = depth + Number(direction[1]);
+      depth += Number(direction[1]);
       break;
     case "forward":
-      horizontal = horizontal + Number(direction[1]);
+      horizontal += Number(direction[1]);
       break;
   }
 });
 
+console.log("--- PART 1 ---");
 console.log("H: ", horizontal);
 console.log("V: ", depth);
 console.log("HxV: ", horizontal * depth);
@@ -40,21 +38,22 @@ let horiz = 0;
 let dep = 0;
 let aim = 0;
 
-dataDict.forEach((direction) => {
+data.forEach((direction) => {
   switch (direction[0]) {
     case "up":
-      aim = aim - Number(direction[1]);
+      aim -= Number(direction[1]);
       break;
     case "down":
-      aim = aim + Number(direction[1]);
+      aim += +Number(direction[1]);
       break;
     case "forward":
-      horiz = horiz + Number(direction[1]);
+      horiz += Number(direction[1]);
       dep = dep + aim * Number(direction[1]);
       break;
   }
 });
 
+console.log("--- PART 2 ---");
 console.log("H: ", horiz);
 console.log("V: ", dep);
 console.log("HxV: ", horiz * dep);
